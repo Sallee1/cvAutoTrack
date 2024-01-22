f83990b4718938bbe50fee6690720fe5ac885134
Sallee <885331635@qq.com>
2024-01-23 02:25:29 +0800

    feat:实现了opencv常见数据结构的序列化方法

--- source/pch.h
+++ source/pch.h
@@ -5,63 +5,63 @@
 // 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
 
 #ifndef PCH_H
-#define PCH_H
+    #define PCH_H
 
-// Opencv
-// 如果使用了c++14以上版本，关闭以下警告
-#ifdef _HAS_CXX17
-    #pragma warning(disable : 4828)
-    #pragma warning(disable : 5054)
-    #pragma warning(disable : 6201)
-    #pragma warning(disable : 6294)
-    #pragma warning(disable : 26451)
-    #pragma warning(disable : 26495)
-#endif
+    // Opencv
+    // 如果使用了c++14以上版本，关闭以下警告
+    #ifdef _HAS_CXX17
+        #pragma warning(disable : 4828)
+        #pragma warning(disable : 5054)
+        #pragma warning(disable : 6201)
+        #pragma warning(disable : 6294)
+        #pragma warning(disable : 26451)
+        #pragma warning(disable : 26495)
+    #endif
 
-#ifdef SUPPORT_WINRT
-    #define WIN32_LEAN_AND_MEAN // 从 Windows 头文件中排除极少使用的内容
-    // Windows 头文件
-    #include <Unknwn.h>
-    #include <inspectable.h>
-    #include <windows.h>
+    #ifdef SUPPORT_WINRT
+        #define WIN32_LEAN_AND_MEAN // 从 Windows 头文件中排除极少使用的内容
+        // Windows 头文件
+        #include <Unknwn.h>
+        #include <inspectable.h>
+        #include <windows.h>
 
-    // WinRT
-    #include <winrt/Windows.Foundation.h>
-    #include <winrt/Windows.Graphics.DirectX.Direct3d11.h>
-    #include <winrt/Windows.Graphics.DirectX.h>
-    #include <winrt/windows.foundation.metadata.h>
+        // WinRT
+        #include <winrt/Windows.Foundation.h>
+        #include <winrt/Windows.Graphics.DirectX.Direct3d11.h>
+        #include <winrt/Windows.Graphics.DirectX.h>
+        #include <winrt/windows.foundation.metadata.h>
 
-    #include <dwmapi.h>
-    #pragma comment(lib, "dwmapi.lib")
-#endif // SUPPORT_WINRT
+        #include <dwmapi.h>
+        #pragma comment(lib, "dwmapi.lib")
+    #endif // SUPPORT_WINRT
 
-// STL
-#include <algorithm>
-#include <atomic>
-#include <chrono>
-#include <filesystem>
-#include <fstream>
-#include <functional>
-#include <future>
-#include <memory>
-#include <mutex>
-#include <numeric>
-#include <random>
-#include <string>
+    // STL
+    #include <algorithm>
+    #include <atomic>
+    #include <chrono>
+    #include <filesystem>
+    #include <fstream>
+    #include <functional>
+    #include <future>
+    #include <memory>
+    #include <mutex>
+    #include <numeric>
+    #include <random>
+    #include <string>
 
-#if (_MSC_VER && _MSVC_LANG <= 201703L) || (!_MSC_VER && __cplusplus <= 201703L)
-    #include <fmt/format.h>
-#else
-    #include <format>
-#endif
+    #if (_MSC_VER && _MSVC_LANG <= 201703L) || (!_MSC_VER && __cplusplus <= 201703L)
+        #include <fmt/format.h>
+    #else
+        #include <format>
+    #endif
 
 namespace global
 {
-#if (_MSC_VER && _MSVC_LANG <= 201703L) || (!_MSC_VER && __cplusplus <= 201703L)
-    #define fmt_namespace_ fmt
-#else
-    #define fmt_namespace_ std
-#endif
+    #if (_MSC_VER && _MSVC_LANG <= 201703L) || (!_MSC_VER && __cplusplus <= 201703L)
+        #define fmt_namespace_ fmt
+    #else
+        #define fmt_namespace_ std
+    #endif
 
     template <typename... Args> std::string format(fmt_namespace_::format_string<Args...> fmt, Args&&... args)
     {
@@ -69,14 +69,14 @@ namespace global
     }
 } // namespace global
 
-// opencv
-#include <opencv2/opencv.hpp>
-#include <opencv2/xfeatures2d.hpp>
+    // opencv
+    #include <opencv2/opencv.hpp>
+    #include <opencv2/xfeatures2d.hpp>
 
-#ifdef SUPPORT_WINDUMP
+    #ifdef SUPPORT_WINDUMP
   // DUMP部分
-    #include "DbgHelp.h"
-    #include "tchar.h"
+        #include "DbgHelp.h"
+        #include "tchar.h"
 inline int GenerateMiniDump(PEXCEPTION_POINTERS pExceptionPointers)
 {
     // 定义函数指针
@@ -126,11 +126,64 @@ inline LONG WINAPI ExceptionFilter(LPEXCEPTION_POINTERS lpExceptionInfo)
     }
     return GenerateMiniDump(lpExceptionInfo);
 }
-    #define INSTALL_DUMP_() SetUnhandledExceptionFilter(ExceptionFilter)
-#else
-    #define INSTALL_DUMP_()
-#endif // SUPPORT_WINDUMP
-#define INSTALL_DUMP(at_func) \
-    INSTALL_DUMP_();          \
-    return at_func
+        #define INSTALL_DUMP_() SetUnhandledExceptionFilter(ExceptionFilter)
+    #else
+        #define INSTALL_DUMP_()
+    #endif // SUPPORT_WINDUMP
+    #define INSTALL_DUMP(at_func) \
+        INSTALL_DUMP_();          \
+        return at_func
 #endif // PCH_H
+
+namespace cereal
+{
+    template <class Archive> inline void serialize(Archive& ar, const cv::Rect2i& rect)
+    {
+        ar(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    template <class Archive> inline void serialize(Archive& ar, const cv::Rect2f& rect)
+    {
+        ar(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    template <class Archive> inline void serialize(Archive& ar, const cv::Rect2d& rect)
+    {
+        ar(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    template <class Archive> inline void serialize(Archive& ar, const cv::Point2i& pt)
+    {
+        ar(pt.x, pt.y);
+    }
+
+    template <class Archive> inline void serialize(Archive& ar, const cv::Point2f& pt)
+    {
+        ar(pt.x, pt.y);
+    }
+
+    template <class Archive> inline void serialize(Archive& ar, const cv::Point2d& pt)
+    {
+        ar(pt.x, pt.y);
+    }
+
+    template <class Archive> inline void serialize(Archive& ar, const cv::KeyPoint kp)
+    {
+        ar(kp.pt.x, kp.pt.y, kp.size, kp.angle, kp.response, kp.octave, kp.class_id);
+    }
+
+    template <class Archive> inline void save(Archive& ar, const cv::Mat& mat)
+    {
+        std::vector<uchar> buf;
+        cv::imencode(".tiff", mat, buf);
+        ar(buf);
+    }
+
+    template <class Archive> inline void load(Archive& ar, cv::Mat& mat)
+    {
+        std::vector<uchar> buf;
+        ar(buf);
+        mat = cv::imdecode(buf, cv::IMREAD_UNCHANGED);
+    }
+
+} // namespace cereal
